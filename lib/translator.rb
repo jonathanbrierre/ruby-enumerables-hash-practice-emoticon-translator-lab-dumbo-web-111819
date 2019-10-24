cfd9b77987e3a62ee19888478a0cfc7f0826a4c8
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-24 18:57:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library (file)
  inner_hash_e = {}
  
  emoticons.each do |key,value|
+   
    emoticons_hash["get_meaning"] = inner_hash_m
    value.each do |(element1, element2)|
       
@@ -19,7 +20,7 @@ def load_library (file)
    emoticons_hash["get_emoticon"] = inner_hash_e
  end 
  emoticons_hash
-binding.pry
+#binding.pry
 end
 
 def get_japanese_emoticon
